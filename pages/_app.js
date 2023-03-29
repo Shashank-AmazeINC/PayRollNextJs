94ea66e4c70b2d0522880dd43b6aecb5ac14d809
Shashank-AmazeINC <125264768+Shashank-AmazeINC@users.noreply.github.com>
2023-03-29 18:10:11 +0530

    Completed UI part of Employee Profile and Position Details

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
           </div>
           <div className='col-10'>
             <Component {...pageProps} />
-            <Footer/>
+           
           </div>
          
         </div>
