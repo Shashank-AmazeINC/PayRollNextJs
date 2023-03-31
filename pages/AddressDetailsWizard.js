a59c2b4411b4046d95ebd2a89df38cbe8378ea0f
Shashank-AmazeINC <125264768+Shashank-AmazeINC@users.noreply.github.com>
2023-03-31 14:50:32 +0530

    re commit

--- pages/AddressDetailsWizard.js
+++ pages/AddressDetailsWizard.js
@@ -1,11 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
+import {StepWizard,S} from "react-step-wizard";
+import EmployeeProfile from './EmployeeProfile';
+import PositionDetails from './PositionDetails';
+import DependentDetails from './DependentDetails';
+import EmploymentHistory from './EmploymentHistory';
+import NominationDetails from './NominationDetails';
+import EducationalAttainment from './EducationalAttainment';
+import BankDetails from './BankDetails';
+import IDDetails from './IDDetails';
+
+
 
 function AddressDetailsWizard() {
     return (
-        <div>
-            <Link href="/EmployeeProfile">EmployeeProfile</Link><br/>
-            <Link href="/PositionDetails">PositionDetails</Link><br/>
+        <div className='container'>
+            
         </div>
     )
 }
